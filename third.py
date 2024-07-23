8b63874e2a07c1cebbf0e6560d59711e2a8d4e0a
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2024-07-23 21:00:48 +0530

    Add files via upload

---
+++ third.py
@@ -0,0 +1,71 @@
+# List
+# marks = [85.6, 65.7, 82.6]
+# print(marks)
+# print(type(marks))
+# print(len(marks))
+# print(marks[1])
+
+# list indexing & mutablity
+# student = ["Priya", 95, 5.6, "Noida"]
+# print(student)
+# print(student[0])
+# student[0] = "kavya"
+# print(student)
+# print(student[6])
+
+# list slicing
+# num = [65,72,16,45,93,33]
+# print(num[2:6])
+# print(num[:5])
+# print(num[2:])
+# print(num[-3:-1])
+
+# list methods
+# num = [65,72,16,45,93,33]
+# num.append(5)
+# num.sort()
+# print(num)
+# num.sort(reverse=True)
+# print(num)
+# num.reverse
+# print(num)
+# num.insert(2,66)
+# print(num)
+# num.remove(33)
+# print(num)
+# num.pop(3)
+# print(num)
+
+# Tuples
+# tup = (6,5,2)
+# print(type(tup))
+# print(tup[1:2])
+# print(tup.index(5))
+# print(tup.count(5))
+
+# Ques!
+# movies = []
+# movies.append(input("Enter 1st movies:"))
+# movies.append(input("Enter 2nd movies:"))
+# movies.append(input("Enter 3rd movies:"))
+# print(movies)
+
+# Ques!
+# num2 = ["m","a","a","m"]
+# num = [6,5,7]
+# num2_copy = num2.copy()
+# num2_copy.reverse()
+# if (num2_copy == num2):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# Ques!
+# tup = ("C", "D", "A", "A", "B", "B", "A")
+# print(tup.count("A"))
+
+# Ques!
+# lis = ["C", "D", "A", "A", "B", "B", "A"]
+# lis.sort()
+# print(lis)
+
