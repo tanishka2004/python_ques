9e18e652f0e4dfbf62a3fec4a866b8f4fef7ec73
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2024-07-26 17:39:09 +0530

    Add files via upload

---
+++ fifth.py
@@ -0,0 +1,44 @@
+# Loops
+# while loops
+# count = 1
+# while count <=100 :
+#     print("hello" , count)
+#     count += 1
+
+# print numbers from 1 to 10
+# i = 1
+# while i <= 10 :
+#     print(i)
+#     i += 1
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# Ques!
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# Ques!
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# Ques!
+# i = 1
+# while i <= 10:
+#     print(2 * i)
+#     i += 1
+
+# Ques!
+# my_list = [1,4,9,16,25,36,64,81,100]
+# i = 0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+
+# Ques!
