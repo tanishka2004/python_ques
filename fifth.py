d6d45427742c23cba74d50fc284251247d49cfac
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2024-07-27 21:32:45 +0530

    Add files via upload

---
+++ fifth.py
@@ -0,0 +1,148 @@
+# Loops
+# while loops
+# count = 1
+# while count <=100 :
+#     print("hello" , count)
+#     count += 1
+
+# print numbers from 1 to 10
+# i = 1
+# while i <= 10 :
+#     print(i)
+#     i += 1
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# Ques!
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# Ques!
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# Ques!
+# i = 1
+# while i <= 10:
+#     print(2 * i)
+#     i += 1
+
+# Ques!
+# my_list = [1,4,9,16,25,36,64,81,100]
+# i = 0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+
+# Ques!
+# nums = (1, 4, 9, 16, 25, 36, 64, 81, 100)
+
+# x = 64
+
+# i = 0
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found at idx" , i)
+        # break #end of the loop
+#     else:
+#         print("fimding..")
+#     i += 1
+
+# i = 1
+# while i < 6:
+#     if(i == 4):
+#        i += 1
+#        continue #skip
+#     print(i)
+#     i += 1
+
+# for loop
+# str = "helloworld"
+# for char in str:
+#     print(char)
+
+# lis = [7,6,4,5]
+# for val in lis:
+#     print(val)
+
+# Ques!
+# lis = [1,4,9,16,25,36,49,64,81,100]
+# for x in lis:
+#     print(x)
+
+# Ques!
+# tup = (1,4,9,16,25,36,49,64,81,100)
+# for x in tup:
+#     if(x == 25):
+#         print("found")
+#     else:    
+#         print(x)
+
+# tup = (1,4,9,16,25,36,49,64,81,100)
+# x = 36
+# idx = 0
+# for y in tup:
+#     if(y == x):
+#         print("found at index", idx)
+#         idx += 1
+
+# range
+# for i in range(10): #range(stop)
+#     print(i)
+
+# for i in range(3 , 10): #range(start , stop)
+#      print(i)
+
+# for i in range(3 , 10 , 2): #range(start , stop , steo)
+#      print(i)
+
+# for i in range(0 , 10, 2):
+#        print(i)
+
+# se = (range(5))
+# for i in se:
+#     print(i)
+
+# Ques!
+# x = int(input("enter the no.:"))
+# for i in range(1 , 11 ):
+#     print(x * i)
+
+# pass
+# for i in range(5):
+#     pass
+# print("some useful work") 
+
+# Ques!
+# x = int(input("enter no:"))
+# sum = 0
+# i = 1
+# while i <= x:
+#     sum += i
+#     i += 1
+
+# print("total sum:", sum)
+
+# Ques!
+# x = int(input("Enter the number"))
+# fact = 1
+# for i in range (1, x+1):
+#     fact *= i
+
+# print("factorial of" , x , "is" , fact)
+
+# x = 5
+# fact = 1
+# i = 1
+# while i <= x:
+#     fact *= i
+#     i += 1  
+
+# print("factorial of" , x , "is" , fact)
