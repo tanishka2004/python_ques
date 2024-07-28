ac96809d0cb23ff19fdf48f62e25524e938ec99c
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2024-07-28 21:11:13 +0530

    Add files via upload

---
+++ sixth.py
@@ -0,0 +1,35 @@
+# Functions
+# functions definition
+# def calsum(a , b): #parameters
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calsum(2 , 3) #function call; arguments
+
+# average of 3 no.
+# def avg(a,b,c):
+#     sum = a+b+c
+#     avg = sum/3
+#     print(avg)
+#     return avg
+# avg(1,2,3)
+
+# def area(a,b,c):
+#     sum = a+b+c
+#     div = sum/3
+#     print(div)
+#     return(div)
+# area(5,4,6)
+
+# default parameters
+# def calsum(a=1 , b=1): 
+#     sum = a + b
+#     print(sum)
+#     return sum
+# calsum()
+cities = ["delhi", "myssore", "hyderabad", "goa"]
+def print_len(cities):
+    print(cities)
+
+print_len(cities)
